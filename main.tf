9deb962f21fff99b81dcb8c56f067507b84a19ae
Cesar Orantes <a.orantes@outlook.com>
2023-12-20 06:05:49 +0000

    adding infrastructure

---
+++ main.tf
@@ -0,0 +1,29 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">= 3.43.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+  skip_provider_registration = true
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "813-3da353c0-hands-on-with-terraform-on-azure"
+  location = "southcentralus"
+  
+}
+
+module "securestorage" {
+  source   = "app.terraform.io/co-lab-env/securestorage/azurerm"
+  version  = "1.0.0"
+  location = azurerm_resource_group.rg.location
+  # insert required variables here
+  resource_group_name  = azurerm_resource_group.rg.name
+  storage_account_name = "stg7676a"
+}
+
